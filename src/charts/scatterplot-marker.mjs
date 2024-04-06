059014fa6bcafa0ab2cc33953c81af9dd1d2a930
José Martínez Santana <jose.martinez.santana@gmail.com>
2024-04-06 12:00:04 -0600

    Added the customization of the marker to be any drawing the svg path d attribute

--- src/charts/scatterplot-marker.mjs
+++ src/charts/scatterplot-marker.mjs
@@ -3,7 +3,11 @@ import ScatterPlot from "./scatterplot-chart.mjs";
 ("use strict");
 
 export default class ScatterPlotMarker extends ScatterPlot {
-  /** @type {{[key: string]: string}} */
+  /**
+   * @description
+   * Object whicj stores some examples of the marker figures to draw in chart.
+   * @type {{[key: string]: string}}
+   * */
   #markers = {
     sun: "M5.114,5.726c0.169,0.168,0.442,0.168,0.611,0c0.168-0.169,0.168-0.442,0-0.61L3.893,3.282c-0.168-0.168-0.442-0.168-0.61,0c-0.169,0.169-0.169,0.442,0,0.611L5.114,5.726z M3.955,10c0-0.239-0.193-0.432-0.432-0.432H0.932C0.693,9.568,0.5,9.761,0.5,10s0.193,0.432,0.432,0.432h2.591C3.761,10.432,3.955,10.239,3.955,10 M10,3.955c0.238,0,0.432-0.193,0.432-0.432v-2.59C10.432,0.693,10.238,0.5,10,0.5S9.568,0.693,9.568,0.932v2.59C9.568,3.762,9.762,3.955,10,3.955 M14.886,5.726l1.832-1.833c0.169-0.168,0.169-0.442,0-0.611c-0.169-0.168-0.442-0.168-0.61,0l-1.833,1.833c-0.169,0.168-0.169,0.441,0,0.61C14.443,5.894,14.717,5.894,14.886,5.726 M5.114,14.274l-1.832,1.833c-0.169,0.168-0.169,0.441,0,0.61c0.168,0.169,0.442,0.169,0.61,0l1.833-1.832c0.168-0.169,0.168-0.442,0-0.611C5.557,14.106,5.283,14.106,5.114,14.274 M19.068,9.568h-2.591c-0.238,0-0.433,0.193-0.433,0.432s0.194,0.432,0.433,0.432h2.591c0.238,0,0.432-0.193,0.432-0.432S19.307,9.568,19.068,9.568 M14.886,14.274c-0.169-0.168-0.442-0.168-0.611,0c-0.169,0.169-0.169,0.442,0,0.611l1.833,1.832c0.168,0.169,0.441,0.169,0.61,0s0.169-0.442,0-0.61L14.886,14.274z M10,4.818c-2.861,0-5.182,2.32-5.182,5.182c0,2.862,2.321,5.182,5.182,5.182s5.182-2.319,5.182-5.182C15.182,7.139,12.861,4.818,10,4.818M10,14.318c-2.385,0-4.318-1.934-4.318-4.318c0-2.385,1.933-4.318,4.318-4.318c2.386,0,4.318,1.933,4.318,4.318C14.318,12.385,12.386,14.318,10,14.318 M10,16.045c-0.238,0-0.432,0.193-0.432,0.433v2.591c0,0.238,0.194,0.432,0.432,0.432s0.432-0.193,0.432-0.432v-2.591C10.432,16.238,10.238,16.045,10,16.045",
     twitter:
@@ -15,15 +19,21 @@ export default class ScatterPlotMarker extends ScatterPlot {
     mars: "m15,7 a 7,7 0 1,0 2,2 z l 1,1 7-7m-7,0 h 7 v 7",
     moon: "m15,3 a 8.5,8.5 0 1,0 0,13 a 6.5,6.5 0 0,1 0,-13",
     star: "m11,1 3,9h9l-7,5.5 2.5,8.5-7.5-5-7.5,5 2.5-8.5-7-6.5h9z",
+    error404: "M3 3h18v18H3zM15 9l-6 6m0-6l6 6",
+    gear: "M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z",
   };
+  #markersConfiguration;
+
   constructor() {
     super();
+    this.#markersConfiguration = undefined;
   }
 
   /**
    * @description
-   * Getter of the svg path to form a figure in the scatter plot.
-   * @returns {{[key: string]: string}}
+   * Getter and setter of new markers to the default object to draw in the chart.
+   * @param {{[key: string]: string}} marker Object which add new figures to draw in the chart. Keys are the figures and values are the svg path.
+   * @returns {{[key: string]: string}|this}
    * @example
    * ```JavaScript
    * // Set all the parameters of the chart
@@ -31,16 +41,57 @@ export default class ScatterPlotMarker extends ScatterPlot {
    *  ...;
    *
    * chart.init();
-   * chart.markers();
+   * chart.markers({
+   *    heart: "M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z",
+   *    instagram: "M16.98 0a6.9 6.9 0 0 1 5.08 1.98A6.94 6.94 0 0 1 24 7.02v9.96c0 2.08-.68 3.87-1.98 5.13A7.14 7.14 0 0 1 16.94 24H7.06a7.06 7.06 0 0 1-5.03-1.89A6.96 6.96 0 0 1 0 16.94V7.02C0 2.8 2.8 0 7.02 0h9.96zm.05 2.23H7.06c-1.45 0-2.7.43-3.53 1.25a4.82 4.82 0 0 0-1.3 3.54v9.92c0 1.5.43 2.7 1.3 3.58a5 5 0 0 0 3.53 1.25h9.88a5 5 0 0 0 3.53-1.25 4.73 4.73 0 0 0 1.4-3.54V7.02a5 5 0 0 0-1.3-3.49 4.82 4.82 0 0 0-3.54-1.3zM12 5.76c3.39 0 6.2 2.8 6.2 6.2a6.2 6.2 0 0 1-12.4 0 6.2 6.2 0 0 1 6.2-6.2zm0 2.22a3.99 3.99 0 0 0-3.97 3.97A3.99 3.99 0 0 0 12 15.92a3.99 3.99 0 0 0 3.97-3.97A3.99 3.99 0 0 0 12 7.98zm6.44-3.77a1.4 1.4 0 1 1 0 2.8 1.4 1.4 0 0 1 0-2.8z"
+   * });
    * ```
    */
-  get markers() {
-    return this.#markers;
+  markers(marker) {
+    if (!arguments.length) {
+      return this.#markers;
+    }
+    if (
+      Object.entries(marker).every(
+        ([key, value]) => typeof key === "string" && typeof value === "string"
+      )
+    ) {
+      this.#markers = { ...this.#markers, ...marker };
+    } else {
+      console.error("The object keys and values must be strings");
+    }
+    return this;
+  }
+
+  /**
+   * @description
+   * Getter of the svg path to form a figure in the scatter plot.
+   * @param {{[key: string]: string}} config The configuration object which the keys are the categories in a series and the values are the svg paths string to figure the marker.
+   * @returns {{[key: string]: string}|this}
+   * @example
+   * ```JavaScript
+   * // Set all the parameters of the chart
+   * const chart = new ScatterPlotMarker()
+   *  ...;
+   *
+   * chart.init();
+   * chart.markersConfig({
+   *    Exp1: "planet",
+   *    Exp2: "star",
+   *    Exp3: "sun"
+   * });
+   * ```
+   */
+  markersConfig(config) {
+    return arguments.length && typeof config === "object"
+      ? ((this.#markersConfiguration = { ...config }), this)
+      : this.#markersConfiguration;
   }
 
   /**
    * @description
    * Creates the data points in the chart.
+   * @param {string} [fillColor="#ddd"] The color to fill the marker. By default the color is "#ddd".
    * @returns {void}
    * @example
    * ```JavaScript
@@ -52,24 +103,13 @@ export default class ScatterPlotMarker extends ScatterPlot {
    * char.addSeries();
    * ```
    */
-  addSeries() {
+  addSeries(fillColor = "#ddd") {
     const seriesGroup = this._svg.append("g").attr("class", "series");
     seriesGroup
       .selectAll(".serie")
       .data(this._ySeriesNames)
       .join("g")
       .attr("class", (d) => `${d.toLowerCase().replace(" ", "-")} serie`);
-
-    /** @type {{[key: string]: string}} */
-    const markers = this.colorScale()
-      .domain()
-      .reduce(
-        (acc, d, i) => ({
-          ...acc,
-          [d]: Object.values(this.markers).at(i),
-        }),
-        {}
-      );
 
     seriesGroup
       .selectAll(".serie")
@@ -80,7 +120,9 @@ export default class ScatterPlotMarker extends ScatterPlot {
           x: this.xValues.at(j),
           y: r[d],
           category: this.categoriesValues.at(j),
-          marker: markers[this.categoriesValues.at(j)],
+          marker:
+            this.markers()[this.markersConfig()[this.categoriesValues.at(j)]] ||
+            this.markers().error404,
         }))
       )
       .join("path")
@@ -93,6 +135,7 @@ export default class ScatterPlotMarker extends ScatterPlot {
       )
       .attr("transform", (d) => `translate(${this.x(d.x)}, ${this.y(d.y)})`)
       .attr("d", (d) => d.marker)
-      .style("fill", (d) => this.colorScale()(d.category));
+      .style("fill", fillColor)
+      .style("stroke", (d) => this.colorScale()(d.category));
   }
 }
